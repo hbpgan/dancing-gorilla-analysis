0dab2a9627808961363869c1ae1bc5534d72041f
hbpgan <2chinose@gmail.com>
2023-03-06 18:55:50 +0900

    add superuser generator

--- bsapps/settings.py
+++ bsapps/settings.py
@@ -138,3 +138,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SUPERUSER_NAME = env("SUPERUSER_NAME")
+SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
+SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
