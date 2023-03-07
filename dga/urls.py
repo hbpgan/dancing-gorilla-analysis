e21e9f7d11a862bd80c8a9a2c1e37486d86698b6
hbpgan <2chinose@gmail.com>
2023-03-07 16:47:18 +0900

    complete result & create qa

--- dga/urls.py
+++ dga/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("analyze_replay/", views.analyze_replay),
     path('result/', views.result, name='result'),
+    path('qa/', views.qa, name='qa')
 ]
--- dga/views.py
+++ dga/views.py
@@ -16,6 +16,10 @@ def index(request):
     form = UrlForm()
     return render(request, 'index.html', {'form': form})
 
+def qa(request):
+    form = UrlForm()
+    return render(request, 'qa.html', {'form': form})
+
 def result(request):
     if request.method == 'GET':
         form = UrlForm(request.GET)
@@ -40,8 +44,7 @@ def analyze_replay(webplayer_url):
     m = make_bsor(f)
     print('BSOR Version: %d' % m.file_version)
     print('BSOR notes: %d' % len(m.notes))
-    print('Song Name (Mapper): {} ({})'.format(
-        m.info.songName, m.info.mapper))
+    print('Song Name (Mapper): {} ({})'.format(m.info.songName, m.info.mapper))
     print('Song Duration: {}sec'.format(round(m.frames[-1].time, 2)))
     print('Song Hash: {}'.format(m.info.songHash))
     print('Song Difficulty: {}'.format(m.info.difficulty))
@@ -76,8 +79,6 @@ def analyze_replay(webplayer_url):
             break
     print('{}~{}'.format(first_note_frame, final_note_frame))
 
-    movement_threshold = 0.0001
-    angle_threshold = 0.1
     for i in range(first_note_frame, final_note_frame):
         cf = m.frames[i]
         pf = m.frames[i-1]
@@ -86,8 +87,7 @@ def analyze_replay(webplayer_url):
         prev_hp = (pf.head.x, pf.head.y, pf.head.z)
         hd = math.dist(hp, prev_hp)
         #hd = ((cf.head.x - pf.head.x)**2 + (cf.head.y - pf.head.y)**2 + (cf.head.z - pf.head.z)**2)**0.5
-        if (hd > movement_threshold):
-            total_head_distance += hd
+        total_head_distance += hd
 
         hq1 = Quaternion(cf.head.x_rot, cf.head.y_rot,
                          cf.head.z_rot, cf.head.w_rot)
@@ -95,14 +95,12 @@ def analyze_replay(webplayer_url):
                          pf.head.z_rot, pf.head.w_rot)
         hq_diff = hq1.inverse * hq2
         hq_deg = abs(hq_diff.degrees)
-        if (hq_deg > angle_threshold):
-            total_head_angle += hq_deg
+        total_head_angle += hq_deg
 
         lp = (cf.left_hand.x, cf.left_hand.y, cf.left_hand.z)
         prev_lp = (pf.left_hand.x, pf.left_hand.y, pf.left_hand.z)
         ld = math.dist(lp, prev_lp)
-        if (hd > movement_threshold):    
-            total_left_distance += ld
+        total_left_distance += ld
 
         lq1 = Quaternion(cf.left_hand.x_rot, cf.left_hand.y_rot,
                          cf.left_hand.z_rot, cf.left_hand.w_rot)
@@ -110,14 +108,12 @@ def analyze_replay(webplayer_url):
                          pf.left_hand.z_rot, pf.left_hand.w_rot)
         lq_diff = lq1.inverse * lq2
         lq_deg = abs(lq_diff.degrees)
-        if (lq_deg > angle_threshold):
-            total_left_angle += lq_deg
+        total_left_angle += lq_deg
 
         rp = (cf.right_hand.x, cf.right_hand.y, cf.right_hand.z)
         prev_rp = (pf.right_hand.x, pf.right_hand.y, pf.right_hand.z)
         rd = math.dist(rp, prev_rp)
-        if (hd > movement_threshold):
-            total_right_distance += rd
+        total_right_distance += rd
 
         rq1 = Quaternion(cf.right_hand.x_rot, cf.right_hand.y_rot,
                          cf.right_hand.z_rot, cf.right_hand.w_rot)
@@ -125,8 +121,7 @@ def analyze_replay(webplayer_url):
                          pf.right_hand.z_rot, pf.right_hand.w_rot)
         rq_diff = rq1.inverse * rq2
         rq_deg = abs(rq_diff.degrees)
-        if (rq_deg > angle_threshold):
-            total_right_angle += rq_deg
+        total_right_angle += rq_deg
 
     record_duration = final_note_time - first_note_time
     nps = len(m.notes) / record_duration
@@ -156,5 +151,57 @@ def analyze_replay(webplayer_url):
     total_hand_angle = total_left_angle + total_right_angle
     gorilla = (total_hand_distance/record_duration)*10 + ((total_hand_angle/record_duration)/nps)/3
     print('ゴリラ指数は{}です。'.format(round(gorilla,2)))
+    
+    # わぁいif文、あかりif文大好き
+    dancer_rank = ""
+    if headbanging > 120:
+        dancer_rank = "超ッ！エキサイティンッ！"
+    elif headbanging > 100:
+        dancer_rank = "超エキサイティング"
+    elif headbanging > 80:
+        dancer_rank = "エキサイティング"
+    elif headbanging > 65:
+        dancer_rank = "ダンシング"
+    elif headbanging > 55:
+        dancer_rank = "かなりノリノリ"
+    elif headbanging > 45:
+        dancer_rank = "ノリノリ"
+    elif headbanging > 35:
+        dancer_rank = "ちょっとノリノリ"
+    elif headbanging > 30:
+        dancer_rank = "ちょっと動く"
+    elif headbanging > 25:
+        dancer_rank = "あまり動かない"
+    elif headbanging > 20:
+        dancer_rank = "ぼったち気味"
+    elif headbanging > 15:
+        dancer_rank = "ぼったち"
+    elif headbanging > 11:
+        dancer_rank = "超ぼったち"
+    elif headbanging > 8:
+        dancer_rank = "動かざること山の如し"
+    else:
+        dancer_rank = "世界樹"
+    
+    gorilla_rank = ""
+    if gorilla > 240:
+        gorilla_rank = "ゴッドゴリラ"
+    elif gorilla > 210:
+        gorilla_rank = "超大暴れゴリラ"
+    elif gorilla > 190:
+        gorilla_rank = "大暴れゴリラ"
+    elif gorilla > 180:
+        gorilla_rank = "暴れゴリラ"
+    elif gorilla > 170:
+        gorilla_rank = "ゴリラ"
+    elif gorilla > 160:
+        gorilla_rank = "ゴリラジェダイ"
+    elif gorilla > 130:
+        gorilla_rank = "ジェダイ"
+    elif gorilla > 110:
+        gorilla_rank = "穏やかジェダイ"
+    else:
+        gorilla_rank = "まったりジェダイ"
+    
     analyzed_data = locals()
     return(analyzed_data)
